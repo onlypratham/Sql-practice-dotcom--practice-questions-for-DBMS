bec59de83a0f8a46c85d22803fdf1aac71033d8c
Pratham Khatri <106012365+onlypratham@users.noreply.github.com>
2026-04-26 00:17:56 +0530

    Update SQL query for patient_id 542's latest admission
    Modified SQL query to retrieve the most recent admission date for patient_id 542.

--- Medium-Questions/17.sql
+++ Medium-Questions/17.sql
@@ -1,8 +1,7 @@
--- Show all of the days of the month (1-31) and how many admission_dates occurred on that day. Sort by the day with most admissions to least admissions.
-
-SELECT
-  DAY(admission_date) AS day_number,
-  COUNT(*) AS number_of_admissions
+-- Show all columns for patient_id 542's most recent admission_date.
+SELECT *
 FROM admissions
-GROUP BY day_number
-ORDER BY number_of_admissions DESC
+WHERE patient_id = 542
+GROUP BY patient_id
+HAVING
+  admission_date = MAX(admission_date);
