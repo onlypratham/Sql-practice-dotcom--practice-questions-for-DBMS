6caf8d13523084ec091408551986310c605115d1
Pratham Khatri <106012365+onlypratham@users.noreply.github.com>
2026-04-25 22:45:01 +0530

    Add SQL query for duplicate patient count
    Query to count duplicate patients based on first and last name.

---
+++ Medium-Questions/1.sql
@@ -0,0 +1,11 @@
+-- display the first name, last name and number of duplicate patients based on their first name and last name.
+
+-- Ex: A patient with an identical name can be considered a duplicate.
+
+select 
+	first_name,
+    last_name,
+    count(*) as num_of_duplicates
+from patients
+Group By first_name, last_name
+having num_of_duplicates>1;
