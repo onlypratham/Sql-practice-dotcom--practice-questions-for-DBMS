5015d8ac45b94ef712bc7d3e97e118d567dc6a29
Pratham Khatri <106012365+onlypratham@users.noreply.github.com>
2026-04-26 00:52:00 +0530

    Add SQL query for patients with 'Epilepsy' diagnosis
    Query retrieves patient details for those diagnosed with 'Epilepsy' and attended by doctor 'Lisa'.

---
+++ Hard-questions/3.sql
@@ -0,0 +1,16 @@
+-- Show patient_id, first_name, last_name, and attending doctor's specialty.
+-- Show only the patients who has a diagnosis as 'Epilepsy' and the doctor's first name is 'Lisa'
+
+-- Check patients, admissions, and doctors tables for required information.
+
+SELECT
+  p.patient_id,
+  p.first_name AS patient_first_name,
+  p.last_name AS patient_last_name,
+  ph.specialty AS attending_doctor_specialty
+FROM patients p
+  JOIN admissions a ON a.patient_id = p.patient_id
+  JOIN doctors ph ON ph.doctor_id = a.attending_doctor_id
+WHERE
+  ph.first_name = 'Lisa' and
+  a.diagnosis = 'Epilepsy'
