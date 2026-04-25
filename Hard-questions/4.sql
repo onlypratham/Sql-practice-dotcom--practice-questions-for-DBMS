d27fe122db69a29e6aec9a375141383fbf70bebc
Pratham Khatri <106012365+onlypratham@users.noreply.github.com>
2026-04-26 00:52:40 +0530

    Add SQL query for patient temporary passwords
    Query to retrieve patient_id and temporary password based on specific criteria.

---
+++ Hard-questions/4.sql
@@ -0,0 +1,16 @@
+-- All patients who have gone through admissions, can see their medical documents on our site. Those patients are given a temporary password after their first admission. Show the patient_id and temp_password.
+
+-- The password must be the following, in order:
+-- 1. patient_id
+-- 2. the numerical length of patient's last_name
+-- 3. year of patient's birth_date
+
+SELECT
+  DISTINCT P.patient_id,
+  CONCAT(
+    P.patient_id,
+    LEN(last_name),
+    YEAR(birth_date)
+  ) AS temp_password
+FROM patients P
+  JOIN admissions A ON A.patient_id = P.patient_id
