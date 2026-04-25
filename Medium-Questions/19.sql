8fc4d5586ec85818dc63cbaa11421315c1c50810
Pratham Khatri <106012365+onlypratham@users.noreply.github.com>
2026-04-26 00:20:24 +0530

    Add SQL query for doctors' admissions count
    Query to retrieve doctors' names and their total admissions attended.

---
+++ Medium-Questions/19.sql
@@ -0,0 +1,11 @@
+-- Show first_name, last_name, and the total number of admissions attended for each doctor.
+
+-- Every admission has been attended by a doctor.
+
+SELECT
+  first_name,
+  last_name,
+  count(*) as admissions_total
+from admissions a
+  join doctors ph on ph.doctor_id = a.attending_doctor_id
+group by attending_doctor_id
