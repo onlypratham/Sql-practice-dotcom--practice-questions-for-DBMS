2f3a23ab5e083095eb92aff8f57a611ba5576898
Pratham Khatri <106012365+onlypratham@users.noreply.github.com>
2026-04-25 23:28:10 +0530

    Add SQL query for unique cities in province 'NS'
    Query to select unique cities from patients in province 'NS'.

---
+++ Easy-questions/14.sql
@@ -0,0 +1,5 @@
+-- Based on the cities that our patients live in, show unique cities that are in province_id 'NS'.
+
+SELECT DISTINCT(city) AS unique_cities
+FROM patients
+WHERE province_id = 'NS';
