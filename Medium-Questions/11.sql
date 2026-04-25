f2638d098857d17f1a5cbcd42e56c69626e3f603
Pratham Khatri <106012365+onlypratham@users.noreply.github.com>
2026-04-26 00:08:28 +0530

    Add SQL query to show allergies by popularity
    This SQL query retrieves all allergies from the patients table, counts their occurrences, filters out NULL values, and orders the results by popularity.

---
+++ Medium-Questions/11.sql
@@ -0,0 +1,10 @@
+-- Show all allergies ordered by popularity. Remove NULL values from query.
+
+SELECT
+  allergies,
+  COUNT(*) AS total_diagnosis
+FROM patients
+WHERE
+  allergies IS NOT NULL
+GROUP BY allergies
+ORDER BY total_diagnosis DESC
