28beabedb3f4ac739772b04a74cec5c8823d9447
Pratham Khatri <106012365+onlypratham@users.noreply.github.com>
2026-04-26 00:06:37 +0530

    Add SQL query to count patients per city
    Query to count patients by city and order results.

---
+++ Medium-Questions/9.sql
@@ -0,0 +1,9 @@
+-- Show the city and the total number of patients in the city.
+-- Order from most to least patients and then by city name ascending.
+
+SELECT
+  city,
+  COUNT(*) AS num_patients
+FROM patients
+GROUP BY city
+ORDER BY num_patients DESC, city asc;
