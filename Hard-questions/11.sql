345f7bd011363a6532807e9f1a66e1282e26e56e
Pratham Khatri <106012365+onlypratham@users.noreply.github.com>
2026-04-26 00:58:31 +0530

    Create SQL query for doctor admissions breakdown
    Add SQL query to breakdown total admissions by doctor per year.

---
+++ Hard-questions/11.sql
@@ -0,0 +1,15 @@
+-- We need a breakdown for the total amount of admissions each doctor has started each year. Show the doctor_id, doctor_full_name, specialty, year, total_admissions for that year.
+
+
+SELECT
+  d.doctor_id as doctor_id,
+  CONCAT(d.first_name,' ', d.last_name) as doctor_name,
+  d.specialty,
+  YEAR(a.admission_date) as selected_year,
+  COUNT(*) as total_admissions
+FROM doctors as d
+  LEFT JOIN admissions as a ON d.doctor_id = a.attending_doctor_id
+GROUP BY
+  doctor_name,
+  selected_year
+ORDER BY doctor_id, selected_year
